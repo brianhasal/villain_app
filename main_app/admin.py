2a69f43169039b5a5861da0766047bb35da9b521
Brian Hasal <brian.hasal@gmail.com>
2022-12-28 16:57:37 -0800

    Added Surveillance model / Connected model to Villain Model / Confirmed connection by testing with ORM

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Villain
+from .models import Villain, Surveillance
 
 # Register your models here.
 
 admin.site.register(Villain)
+admin.site.register(Surveillance)
 
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.urls import reverse
 
+S_WINDOWS = (
+  ('M', 'Morning'),
+  ('A', 'Afternoon'),
+  ('E', 'Evening'),
+  ('N', 'Night')
+)
+
 # Create your models here.
 
 class Villain(models.Model):
@@ -15,6 +22,22 @@ class Villain(models.Model):
 
   def get_absolute_url(self):
     return reverse('detail', kwargs={ 'villain_id': self.id })
+
+class Surveillance(models.Model):
+  date = models.DateField('Surveillance Date: ')
+  s_window = models.CharField(
+    max_length=1,
+    choices=S_WINDOWS,
+    default=S_WINDOWS[0][0]
+  )
+
+  villain = models.ForeignKey(Villain, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_s_window_display()} on {self.date}"
+
+
+
 
 
 
